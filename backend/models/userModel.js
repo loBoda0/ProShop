dd545516e1f3217d1190ed1306132143ea808e9c
Martin <martin.loboda02@gmail.com>
2022-01-26 21:05:12 +0100

    Backend for authentication all set

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
 
 const userSchema = mongoose.Schema({
     name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
         type: String,
         required: true,
         unique: true
@@ -17,6 +23,19 @@ const userSchema = mongoose.Schema({
     },
 }, {
     timestamps: true
+})
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) {
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 const User = mongoose.model('User', userSchema)
